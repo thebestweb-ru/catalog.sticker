339fa85c9b23587522fb3d74181fca06ea2ea590
thebestweb-ru <ivan_fedya@mail.ru>
2018-11-27 19:24:05 +0300

    Страница админики "Добавление группы стикеров" частично готова.

--- admin/tbw_catalog_sticker_list_item.php
+++ admin/tbw_catalog_sticker_list_item.php
@@ -1,7 +1,8 @@
 <?
-use \Bitrix\Main\Localization\Loc;
-use Bitrix\Main\Loader;
-use \Oceandevelop\Wishlist\WishlistProListTable;
+use Bitrix\Main\Localization\Loc,
+    Bitrix\Main\Loader,
+    TheBestWeb\CatalogSticker\ListTable,
+    TheBestWeb\CatalogSticker\ListSectionsTable;
 
 require_once ($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
 
@@ -9,67 +10,138 @@ Loc::loadMessages(__FILE__);
 
 global $USER, $APPLICATION, $DB;
 
-$module_id = 'oceandevelop.wishlist';
+$MODULE_ID = 'thebestweb.catalog.sticker';
+$MODULE_LANG_PREFIX = 'TBW_CATALOG_STICKER';
 
-
-if (!Loader::includeModule($module_id))
+if (!Loader::includeModule($MODULE_ID))
 {
-    $APPLICATION->ThrowException(GetMessage("MODULE_WISHLIST_NOT_INSTALL"));
+    $APPLICATION->ThrowException(Loc::getMessage($MODULE_LANG_PREFIX."_NOT_INSTALL"));
     return false;
 }
+
 if (!Loader::includeModule('iblock'))
 {
-    $APPLICATION->ThrowException(GetMessage("MODULE_WISHLIST_IBLOCK_NOT_INSTALL"));
+    $APPLICATION->ThrowException(Loc::getMessage($MODULE_LANG_PREFIX."_IBLOCK_NOT_INSTALL"));
     return false;
 }
 
-$POST_RIGHT = $APPLICATION->GetGroupRight($module_id);
+$POST_RIGHT = $APPLICATION->GetGroupRight($MODULE_ID);
 if($POST_RIGHT=="D")
-    $APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
-
-$sTableID=WishlistProListTable::getTableName();
+    $APPLICATION->AuthForm(Loc::getMessage("ACCESS_DENIED"));
 
 
-$aTabs[]=array("DIV" => "edit1", "TAB" => GetMessage("MODULE_WISHLIST_TAB"), "ICON"=>"main_user_edit", "TITLE"=>GetMessage("MODULE_WISHLIST_TAB"));
+
+$rsSites = CSite::GetList($by="sort", $order="desc", Array());
+while ($arSite = $rsSites->Fetch())
+{
+    $arSites[$arSite['LID']] = '['.$arSite['LID'].'] '.$arSite['NAME'];
+}
+
+unset($rsSites);
+unset($arSite);
+
+$sTableID=ListTable::getTableName();
+
+$aTabs[]=array("DIV" => "edit1", "TAB" => Loc::getMessage($MODULE_LANG_PREFIX."_TAB"), "ICON"=>"main_user_edit", "TITLE"=>Loc::getMessage($MODULE_LANG_PREFIX."_TAB"));
 $tabControl = new CAdminTabControl("tabControl", $aTabs);
 
 $ID = intval($ID);
+$message = null;		// сообщение об ошибке
+$bVarsFromForm = false; // флаг "Данные получены с формы", обозначающий, что выводимые данные получены с формы, а не из БД.
 
-// выборка данных
-if($ID>0)
+if(
+    $REQUEST_METHOD == "POST" // проверка метода вызова страницы
+    &&
+    ($save!="" || $apply!="") // проверка нажатия кнопок "Сохранить" и "Применить"
+    &&
+    $POST_RIGHT=="W"          // проверка наличия прав на запись для модуля
+    &&
+    check_bitrix_sessid()     // проверка идентификатора сессии
+)
 {
-    $result = WishlistProListTable::GetByID($ID);
+
+    if($SETTINGS['CRM']=='Y'){
+        if(empty($SETTINGS['CRM_URL']) || empty($SETTINGS['CRM_LOGIN']) || empty($SETTINGS['CRM_PASSWORD']))
+            $message = new CAdminMessage(array(
+                'MESSAGE' =>  GetMessage("MODULE_WISHLIST_CRM_NOT_REQUIRED_FIELDS"),
+                'TYPE' => 'ERROR',
+                'DETAILS' => '',
+                'HTML' => true
+            ));
+    }
+
+    if(empty($SETTINGS['EVENT_ID']))
+        $message = new CAdminMessage(array(
+            'MESSAGE' =>  GetMessage("MODULE_WISHLIST_SETTINGS_NOT_EMAIL_EVENT"),
+            'TYPE' => 'ERROR',
+            'DETAILS' => '',
+            'HTML' => true
+        ));
+
+    $SETTINGS['EVENT_NAME']='MODULE_WISHLIST_SEND_LIST';
+    // обработка данных формы
+    $arFields = Array(
+        "ID"                => $ID,
+        "SITE_ID"           => $SITE_ID,
+        "SETTINGS"           => $SETTINGS,
+    );
+
+    if(!$message){
+        // сохранение данных
+        if($action =='add')
+        {
+            $result = WishlistProOptionsTable::add($arFields);
+            if (!$result->isSuccess())
+            {
+                $message = $result->getErrorMessages();
+            }
+
+        }
+        else
+        {
+            $result = WishlistProOptionsTable::update($ID,$arFields);
+            if (!$result->isSuccess())
+            {
+                $message = $result->getErrorMessages();
+            }
+        }
+    }
+
+    if($result)
+    {
+        // если сохранение прошло удачно - перенаправим на новую страницу
+        // (в целях защиты от повторной отправки формы нажатием кнопки "Обновить" в браузере)
+        if ($apply != "")
+            // если была нажата кнопка "Применить" - отправляем обратно на форму.
+            LocalRedirect("/bitrix/admin/oceandevelop_wishlist_options.php?lang=".LANG."&site=".$SITE_ID."&mess=ok&".$tabControl->ActiveTabParam());
+        else
+            // если была нажата кнопка "Сохранить" - отправляем к списку элементов.
+            LocalRedirect("/bitrix/admin/oceandevelop_wishlist_options.php?lang=".LANG."&site=".$SITE_ID."&mess=ok&".$tabControl->ActiveTabParam());
+    }
+    else
+    {
+        // если в процессе сохранения возникли ошибки - получаем текст ошибки и меняем вышеопределённые переменные
+        if($e = $APPLICATION->GetException())
+            $message = new CAdminMessage(GetMessage("MODULE_WISHLIST_SETTINGS_SAVE_ERROR"), $e);
+        $bVarsFromForm = true;
+    }
+}
+// выборка данных
+if($ID>0 )
+{
+    $result = ListTable::GetById($ID);
     if($Item=$result->fetch()){
         $ID=$Item['ID'];
         $bVarsFromForm = true;
-        $WISHLIST=new ODWishlist($Item['WISHLIST_CODE']);
-        $ITEMS=$WISHLIST->getWishlistItems(true);
-        $WISHLIST=$WISHLIST->getWishlist();
     }
 }
 
-$products_column=array(
-        'ID'=>'ID',
-        'NAME'=>'Название',
-        'ACTIVE'=>'Активность',
-        'PREVIEW_PICTURE'=>'Изображение',
-        'CML2_ARTICLE'=>'Артикул',
-        'AVAILABLE'=>'Доступен к покупке',
-);
-foreach($ITEMS as $ITEM){
-    $products_column_variable[]=array(
-        'ID'=>$ITEM['ID'],
-        'NAME'=>'<a href="/bitrix/admin/iblock_element_edit.php?IBLOCK_ID='.$ITEM['IBLOCK_ID'].'&type=catalog&ID='.$ITEM['ID'].'&lang='.LANG.'">'.$ITEM['NAME'].'</a>',
-        'ACTIVE'=>$ITEM['ACTIVE'],
-        'PREVIEW_PICTURE'=>'<img src="'.$ITEM['PREVIEW_PICTURE']['SRC'].'" width="200">',
-        'CML2_ARTICLE'=>$ITEM['CML2_ARTICLE'],
-        'AVAILABLE'=>$ITEM['CATALOG']['AVAILABLE'],
-    );
-}
-
-$APPLICATION->SetTitle(($ID>0? GetMessage("MODULE_WISHLIST_TITLE").'['.$ID.'] '.$Item['NAME'] : GetMessage("MODULE_WISHLIST_TITLE")));
+$APPLICATION->SetTitle(($ID>0? Loc::getMessage($MODULE_LANG_PREFIX."_TITLE_EDIT").'['.$ID.'] '.$Item['NAME'] : Loc::getMessage($MODULE_LANG_PREFIX."_TITLE_ADD")));
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_after.php");
+
+if($_REQUEST["mess"] == "ok" && !empty($SITE_ID))
+    CAdminMessage::ShowMessage(array("MESSAGE"=>GetMessage("MODULE_WISHLIST_SETTINGS_SAVE_OK"), "TYPE"=>"OK"));
 
 if($message)
     echo $message->Show();
@@ -77,93 +149,57 @@ if($message)
 ?>
     <form method="POST" Action="<?echo $APPLICATION->GetCurPage()?>" ENCTYPE="multipart/form-data" name="post_form">
         <?echo bitrix_sessid_post();?>
+        <input type="hidden" name="lang" value="<?=LANG?>">
+        <input type="hidden" name="action" value="<?=$bVarsFromForm ? 'update' : 'add';?>">
+        <input type="hidden" name="ID" value="<?=$ID ? $ID : '';?>">
+
         <?
-        // отобразим заголовки закладок
         $tabControl->Begin();
-        ?>
-        <?
-        //Вкладка ОСНОВНЫЕ
         $tabControl->BeginNextTab();
         ?>
+        <?if(!empty($arSites)):?>
+            <?foreach ($arSites as $lid=>$site_name):?>
+
+            <?endforeach;?>
+        <?endif;?>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_SITE_ID")?></td>
-            <td><label><?=$WISHLIST['SITE_ID'];?></label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_NAME")?></td>
+            <td><input type="text" name="NAME" value="<?=$Item['NAME'];?>" size="30" maxlength="100" ></td>
         </tr>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_DATE_CREATE")?></td>
-            <td><label><?=$WISHLIST['DATE_CREATE']->toString();?></label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_DATE_START")?></td>
+            <td><input type="text" name="DATE_START" value="<?=$Item['DATE_START'];?>" size="30" maxlength="100" ></td>
         </tr>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_DATE_CHANGE")?></td>
-            <td><label><?=$WISHLIST['DATE_CHANGE']->toString();?></label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_DATE_END")?></td>
+            <td><input type="text" name="DATE_END" value="<?=$Item['DATE_END'];?>" size="30" maxlength="100" ></td>
         </tr>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_USER_ID")?></td>
-            <td><label>
-                    <?if($WISHLIST['USER_ID']):?>
-                    <a href="user_edit.php?ID=<?=$WISHLIST['USER_ID'];?>&lang=<?=LANG?>"><?=$WISHLIST['USER_ID'];?></a>
-                    <?else:?>
-                    <?=GetMessage("MODULE_WISHLIST_LIST_ANONIM")?>
-                    <?endif;?>
-                </label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_ACTIVE")?></td>
+            <td><?=InputType("checkbox", "ACTIVE", "Y", '',  '','',$Item['ACTIVE'] ? 'checked':'');?>
         </tr>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_USER_NAME")?></td>
-            <td><label><?=$WISHLIST['USER_NAME'];?></label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_SORT")?></td>
+            <td><input type="text" name="SORT" value="<?=$Item['SORT'];?>" size="30" maxlength="100" ></td>
         </tr>
         <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_USER_EMAIL")?></td>
-            <td><label><?=$WISHLIST['USER_EMAIL'];?></label></td>
-        </tr>
-        <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_USER_PHONE")?></td>
-            <td><label><?=$WISHLIST['USER_PHONE'];?></label></td>
-        </tr>
-        <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_WISHLIST_CODE")?></td>
-            <td><label><?=$WISHLIST['WISHLIST_CODE'];?></label></td>
-        </tr>
-        <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_SEND_TO_CRM")?></td>
-            <td><label><?=$WISHLIST['SEND_TO_CRM'] ? GetMessage("MODULE_WISHLIST_LIST_YES") :GetMessage("MODULE_WISHLIST_LIST_NO");?></label></td>
-        </tr>
-        <tr>
-            <td><?=GetMessage("MODULE_WISHLIST_LIST_SEND_TO_EMAIL")?></td>
-            <td><label><?=$WISHLIST['SEND_TO_EMAIL'] ? GetMessage("MODULE_WISHLIST_LIST_YES") :GetMessage("MODULE_WISHLIST_LIST_NO");?></label></td>
+            <td><?=Loc::getMessage($MODULE_LANG_PREFIX."_FIELD_TYPE")?></td>
+            <td><input type="text" name="TYPE" value="<?=$Item['TYPE'];?>" size="30" maxlength="100" ></td>
         </tr>
         <?
+        // завершение формы - вывод кнопок сохранения изменений
+        $tabControl->Buttons(
+            array(
+                "disabled"=>($POST_RIGHT<"W"),
+                "back_url"=>"rubric_admin.php?lang=".LANG,
+            )
+        );
         // завершаем интерфейс закладки
         $tabControl->End();
-        ?>
-
-            <table class="adm-s-order-table-ddi-table" style="width: 100%; text-align: center;" id="sale_order_view_product_table">
-                <thead >
-                <tr>
-                    <?foreach($products_column as $colId => $name):?>
-                       <td><?=htmlspecialcharsbx($name)?></td>
-                    <?endforeach;?>
-                </tr>
-                </thead>
-                <tbody style="border: 1px solid rgb(221, 221, 221);" id="sale-adm-order-basket-loading-row">
-                <?foreach($products_column_variable as $key_item=>$item):?>
-                <tr>
-                    <?$i=0;
-                    foreach($item as $key=>$value):?>
-                        <td>
-                           <?=$value?>
-                        </td>
-                    <?endforeach;?>
-                </tr>
-                <?endforeach;?>
-                </tbody>
-
-            </table>
-        <input type="hidden" name="lang" value="<?=LANG?>">
-        <?if($ID>0 && !$bCopy):?>
-            <input type="hidden" name="ID" value="<?=$ID?>">
-        <?endif;?>
-        <?
         $tabControl->ShowWarnings("post_form", $message);
         ?>
+        <?echo BeginNote();?>
+        <span class="required">*</span><?echo GetMessage("REQUIRED_FIELDS")?>
+        <?echo EndNote();?>
     </form>
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_admin.php"); ?>
--- lang/ru/admin/tbw_catalog_sticker_list_item.php
+++ lang/ru/admin/tbw_catalog_sticker_list_item.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bitrixprog
- * Date: 27.11.2018
- * Time: 13:11
- */
+$MESS["TBW_CATALOG_STICKER_NOT_INSTALL"] = "Модуль TBW Стикеры в каталоге не установлен";
+$MESS["TBW_CATALOG_STICKER_IBLOCK_NOT_INSTALL"] = "Модуль Инфоблоки не установлен";
+$MESS["TBW_CATALOG_STICKER_NOT_FIND_SITE_ID"] = "Не найден идентификатор сайта";
+$MESS["TBW_CATALOG_STICKER_TAB"] = "Основные";
+$MESS["TBW_CATALOG_STICKER_TITLE_EDIT"] = "Редактирование группы стикеров";
+$MESS["TBW_CATALOG_STICKER_TITLE_ADD"] = "Создание группы стикеров";
